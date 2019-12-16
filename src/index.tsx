c92991a0f43ce6a5a34f1aa6418aa2bac4a14c79
codiechanel <codiechanel@gmail.com>
2019-12-16 17:06:34 +0800

    Initial commit

---
+++ src/index.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+// import { App } from './indexSpring';
+import { App } from './index3';
+
+ReactDOM.render(
+	<App />,
+	document.getElementById('root')
+	// <Hello compiler="TypeScript" framework="React" />,
+	// document.getElementById("root")
+);
